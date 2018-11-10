e0331d5c724c750c29a695033f2f0d62061f9fb6
Vitalik Hashchuk <vitalikhashchuk@gmail.com>
2018-11-11 01:34:10 +0200

    Update UI. Prepared for DI implementation

--- Launcher/MainWindow.xaml.cs
+++ Launcher/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using Brushes = System.Windows.Media.Brushes;
 using MouseEventArgs = System.Windows.Forms.MouseEventArgs;
 
 namespace Launcher
@@ -9,16 +14,47 @@ namespace Launcher
     /// </summary>
     public partial class MainWindow
     {
-        protected System.Windows.Forms.NotifyIcon MyNotifyIcon;
+        private System.Windows.Forms.NotifyIcon MyNotifyIcon;
+
+        private List<char> _emailAllowedSymbols;
+        private List<char> _usernameAllowedSymbols;
+        private List<char> _digits;
+
+        private bool _regUsernameGood;
+        private bool _regEmailGood;
+        private bool _regPasswordGood;
 
         public MainWindow()
         {
             InitializeComponent();
             MyNotifyIcon = new System.Windows.Forms.NotifyIcon
             {
-                Icon = new System.Drawing.Icon("launcher.ico")
+                Icon = new Icon("launcher.ico")
             };
             MyNotifyIcon.MouseClick += MyNotifyIcon_OnMouseClick;
+
+            _emailAllowedSymbols = new List<char>();
+            for (var c = 'A'; c <= 'Z'; c++)
+            {
+                _emailAllowedSymbols.Add(c);
+            }
+            for (var c = 'a'; c <= 'z'; c++)
+            {
+                _emailAllowedSymbols.Add(c);
+            }
+            _digits = new List<char>();
+            for (var c = '0'; c <= '9'; c++)
+            {
+                _emailAllowedSymbols.Add(c);
+                _digits.Add(c);
+            }
+            _emailAllowedSymbols.Add('@');
+            _emailAllowedSymbols.Add('.');
+
+            _usernameAllowedSymbols = new List<char>(_emailAllowedSymbols);
+            _usernameAllowedSymbols.Remove('@');
+            _usernameAllowedSymbols.Remove('.');
+
         }
 
         private void MyNotifyIcon_OnMouseClick(object sender, MouseEventArgs e)
@@ -33,12 +69,12 @@ namespace Launcher
                 DragMove();
         }
 
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
         }
 
-        private void CollapseButton_Click(object sender, RoutedEventArgs e)
+        private void CollapseButton_OnClick(object sender, RoutedEventArgs e)
         {
             MyNotifyIcon.Visible = true;
             Hide();
@@ -57,17 +93,181 @@ namespace Launcher
             }
         }
 
-        private void LoginGrid_OnKeyDown(object sender, KeyEventArgs e)
+        private void LoginGrid_OnKeyUp(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Enter && !RememberMeCheckBox.IsFocused)
+            if (UsernameTextBox.Text.Length > 0 && UserPasswordBox.Password.Length > 0)
             {
-                SignInButton.Content = "";
+                SignInButton.IsEnabled = true;
+            }
+            else
+            {
+                SignInButton.IsEnabled = false;
             }
         }
 
         private void SignInButton_OnClick(object sender, RoutedEventArgs e)
         {
             SignInButton.Content = "";
+        }
+
+        private void RegisterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            LoginGrid.Visibility = Visibility.Collapsed;
+            LoginGrid.IsEnabled = false;
+            RegisterGrid.Visibility = Visibility.Visible;
+            RegisterGrid.IsEnabled = true;
+        }
+
+        private void RegisterGrid_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (_regEmailGood && _regPasswordGood && _regUsernameGood)
+            {
+                SignUpButton.IsEnabled = true;
+            }
+            else
+            {
+                SignUpButton.IsEnabled = false;
+            }
+        }
+
+        private bool RegPasswordCheck()
+        {
+            if (RegUserPasswordBox.Password.Length < 6)
+            {
+                _regPasswordGood = false;
+                return false;
+            }
+            _regPasswordGood = RegUserPasswordBox.Password == RegUserPasswordCheckBox.Password;
+            return _regPasswordGood;
+        }
+
+        private void RegPasswordCheckColor(bool check)
+        {
+            if (!check)
+            {
+                var brush = Brushes.Red.CloneCurrentValue();
+                brush.Opacity = 0.5;
+                RegUserPasswordBox.Background = brush;
+                RegUserPasswordCheckBox.Background = brush;
+            }
+            else
+            {
+                RegUserPasswordBox.Background = UsernameTextBox.Background;
+                RegUserPasswordCheckBox.Background = UsernameTextBox.Background;
+            }
+        }
+
+        private void RegUserPasswordCheckBox_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            RegPasswordCheckWatermark.Visibility = RegUserPasswordCheckBox.Password == "" ? Visibility.Visible : Visibility.Collapsed;
+            RegPasswordCheckColor(RegPasswordCheck());
+        }
+
+        private void RegUserPasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            RegPasswordWatermark.Visibility = RegUserPasswordBox.Password == "" ? Visibility.Visible : Visibility.Collapsed;
+            RegPasswordCheckColor(RegPasswordCheck());
+        }
+
+        private void SignUpButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            SignUpButton.Content = "";
+        }
+
+        private void RegisterBackButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            RegisterGrid.Visibility = Visibility.Collapsed;
+            RegisterGrid.IsEnabled = false;
+            LoginGrid.Visibility = Visibility.Visible;
+            LoginGrid.IsEnabled = true;
+        }
+
+        private bool RegCheckEmail()
+        {
+            var email = RegEmailTextBox.Text;
+            if (email == "")
+            {
+                _regEmailGood = false;
+                return true;
+            }
+            var check = email.Contains('@') && email.Contains('.');
+            if (check)
+            {
+                foreach (var c in email)
+                {
+                    if (_emailAllowedSymbols.Contains(c)) continue;
+                    check = false;
+                    break;
+                }
+            }
+            _regEmailGood = check;
+            return check;
+        }
+
+        private void RegCheckEmailColor(bool check)
+        {
+            if (check)
+            {
+                RegEmailTextBox.Background = UsernameTextBox.Background;
+            }
+            else
+            {
+                var brush = Brushes.Red.CloneCurrentValue();
+                brush.Opacity = 0.5;
+                RegEmailTextBox.Background = brush;
+            }
+        }
+
+        private void RegEmailTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            RegCheckEmailColor(RegCheckEmail());
+        }
+
+        private bool RegUsernameCheck()
+        {
+            var username = RegUsernameTextBox.Text;
+            if (username == "")
+            {
+                _regUsernameGood = false;
+                return true;
+            }
+
+            if (_digits.Contains(username[0]))
+            {
+                _regUsernameGood = false;
+                return false;
+            }
+            var check = username.Length >= 3;
+            if (check)
+            {
+                foreach (var c in username)
+                {
+                    if (_usernameAllowedSymbols.Contains(c)) continue;
+                    check = false;
+                    break;
+                }
+            }
+            _regUsernameGood = check;
+            return check;
+        }
+
+        private void RegUsernameCheckColor(bool check)
+        {
+            if (check)
+            {
+                RegUsernameTextBox.Background = UsernameTextBox.Background;
+            }
+            else
+            {
+                var brush = Brushes.Red.CloneCurrentValue();
+                brush.Opacity = 0.5;
+                RegUsernameTextBox.Background = brush;
+            }
+        }
+
+        private void RegUsernameTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            RegUsernameCheckColor(RegUsernameCheck());
         }
     }
 }
